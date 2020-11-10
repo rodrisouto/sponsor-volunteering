d35404ad023ed36704ce152ffca90e8131766583
Nacho Raik <ignacioraiksg@gmail.com>
2020-11-10 17:40:27 -0300

    Pick location at loading form

--- sponsor_volunteering/lib/main.dart
+++ sponsor_volunteering/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sponsor Volunteering',
-      home: MapPage(),
+      home: LoadSponsoreePage(),
     );
   }
 }
